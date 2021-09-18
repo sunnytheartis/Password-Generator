81dab35b8d193d20c03fad71e4762c930e6032c6
Sun <solskinartwork@gmail.com>
2021-09-17 23:02:54 -0400

    adding functionality to the website

--- Develop/script.js
+++ Develop/script.js
@@ -10,12 +10,20 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   console.log("Button has been clicked");
 var allSelect = [];
+var password = "";
+var charLength = NaN;
 var charLength = window.prompt("How many characters for your password?")
 
-if (charLength<8 || charLength>128){
+if (charLength < 8 || charLength > 128){
   window.alert("Please select length between 8 & 128 characters");
   generatePassword();
-} 
+} else if ((charLength >=8) && (charLength <= 128) ){
+  
+}else {
+  window.alert("Please enter valid number.")
+  generatePassword();
+}
+
 console.log(charLength);
 
 var upperC = window.confirm('Would you like capital letters?');
@@ -24,29 +32,34 @@ var specC = window.confirm("Would you like special characters?");
 var numbr = window.confirm("Would like any numbers?");
 
 if (!specC && !lowerC && !numbr && !upperC){ window.alert("Select at least one option")
- generatePassword();
+ return password;
 
 }
 if (upperC){
   var uppercase = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   allSelect = [...uppercase,...allSelect];
+  return;
 }if (lowerC){
   var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   allSelect = [...lowercase,...allSelect];
+  return;
   
 } if (specC){
   var specialC = ['~','!','@','#','$','%','^','&','*','(',')','_','+']
   allSelect = [...specialC,...allSelect];
+  return;
 }if (numbr){
   var numbers = ['1','2','3','4','5','6','7','8','9','0']
   allSelect = [...numbers,...allSelect];
+  return;
 }
 
  for (var i =0; i < charLength; i++){
-   Math.floor(Math.random()*allSelect.length)
+   var rando = Math.floor(Math.random()*allSelect.length);
+   password += allSelect[rando];
  }
- 
-  return( "generated password");
+ console.log(password)
+  return password;
 
 }
 
